7cb1046842580be71f1a853333889943b1a8bf6f
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-16 12:15:42 -0400

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,3 +2,12 @@
 from clarifai import rest
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
+
+app = ClarifaiApp(api_key='{gotta get ur own}')
+
+model = app.models.get('general-v1.3')
+
+images = ClImage(file_obj=open('[LOCAL PATH TO IMAGE HERE].jpeg', 'rb'))
+
+data = model.predict([images])
+
