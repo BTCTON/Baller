061a4156e760a71f7de286e1f108c99bc37b730f
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-16 12:14:02 -0400

    added import statements

---
+++ predict.py
@@ -0,0 +1,4 @@
+########## Classifier using the Clarifai API for fun ########################
+from clarifai import rest
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
