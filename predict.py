898367119b7c7afb509c33e221146cf6380be9bb
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-16 12:16:39 -0400

    finished it

--- predict.py
+++ predict.py
@@ -1,4 +1,6 @@
 ########## Classifier using the Clarifai API for fun ########################
+
+## Can be used for anything not just the different types of balls as the functions are provided by Clarifai and are well trained! #######
 from clarifai import rest
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
@@ -11,3 +13,10 @@ images = ClImage(file_obj=open('[LOCAL PATH TO IMAGE HERE].jpeg', 'rb'))
 
 data = model.predict([images])
 
+accuracy = data['outputs'][0]['data']['concepts'][0]['value']
+
+label = data['outputs'][0]['data']['concepts'][0]['name']
+
+percent = accuracy*100
+
+print("The object you gave me is a {}, and I am {} percent confident!").format(label, percent)
